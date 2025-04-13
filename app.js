d37f21fb668cbd551d8ede71ae1b1e047bb1ea00
Sushil Kumar <ksk.sushil.kumar@gmail.com>
2025-04-13 16:05:34 +0000

    client and server side tracking

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('appInsights', appInsights.defaultClient);
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let appInsights = req.app.get('appInsights')
+  appInsights.trackEvent({ name: "serverCount" })
   res.render('index', { title: 'Express' });
 });
 
